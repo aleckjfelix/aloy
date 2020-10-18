d6aa3e3a379bb6b4587e86d2c04e25cad69e8782
Alec Felix <aleckjfelix@gmail.com>
2020-10-18 15:18:37 -0400

    BLE Devices List
    Added display of BLE devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,182 @@
+
+/*
+ * main method
+ */
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 void main() {
   runApp(MyApp());
-}
+}//main method
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    return MaterialApp(home: HomeScreen());
+  }//build
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+}//MyApp
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/*
+ * HomePage
+ * States: off, on, Music, Lightshow;
+ * Nav to: Menu
+ */
+class HomeScreen extends StatefulWidget {
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> devices = new List<BluetoothDevice>();
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  State<StatefulWidget> createState() {
+    return _HomeScreenLedOnState();
+  }//build
+}//HomePage
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+/*
+ * HomeScreenLedOnState
+ * What HomeScreen looks like with LED ON
+ */
+class _HomeScreenLedOnState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          appBar: AppBar(
+              title: const Text('Aloy'),
+              leading: Builder(
+                builder: (context) =>  IconButton(
+                    icon: Icon(Icons.menu, color:Colors.black),
+                    onPressed: () => Scaffold.of(context).openDrawer()
+                )
+              ),
+              actions: <Widget>[IconButton(
+                  icon: Icon(Icons.power_settings_new),
+                  onPressed: null
+              )],
+              centerTitle: true,
+              backgroundColor: Colors.white
+          ),
+          body:
+              Column(
+                children: <Widget>[
+
+                ],
+              ),
+          bottomNavigationBar: BottomAppBar(
+              color: Colors.grey,
+              elevation: 0,
+              child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.music_note),
+                        onPressed: null
+                    ),
+                    IconButton(
+                        icon: Icon(Icons.wb_incandescent_outlined),
+                        onPressed: null
+                    )
+                  ]
+              )
+          ),
+          drawer: Drawer(
+            child: _buildListViewOfDevices()
+          ),
+      );
+  }//build
+
+  _addDeviceToList(final BluetoothDevice device){
+    if(!widget.devices.contains(device)){
+      setState(() {
+        widget.devices.add(device);
+      });
+    }//list does'nt contain device
+  }//_addDeviceToList
+
+  @override
+  void initState(){
+    super.initState();
+    widget.flutterBlue.connectedDevices.asStream().listen((List<BluetoothDevice> devices) {
+      for(BluetoothDevice device in devices)
+        _addDeviceToList(device);
+    });
+    widget.flutterBlue.scanResults.listen((List<ScanResult> results){
+      for(ScanResult result in results) {
+        _addDeviceToList(result.device);
+      }
+    });
+  }//initState
+
+  ListView _buildListViewOfDevices() {
+    List<Widget> containers = new List<Widget>();
+    containers.add(DrawerHeader(
+      child: Text('Devices'),
+      decoration: BoxDecoration(
+        color: Colors.white
+      )
+    ));
+    for (BluetoothDevice device in widget.devices) {
+      containers.add(
+        Container(
+          height: 50,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Text(device.name == '' ? '(unknown device)' : device.name),
+                    Text(device.id.toString()),
+                  ],
+                ),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                child: Text(
+                  'Connect',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    }
+
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        ...containers,
+      ],
     );
   }
-}
+
+}//_HomeScreenLedOffState
+
+/*
+ * HomeScreenLedOnState
+ * What HomeScreen looks like with LED OFF
+ */
+class _HomeScreenLedOffState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }//build
+
+}//_HomeScreenLedOffState
+
+
+/*
+ * Devices Page
+ * View a list of available devices
+ * Connect to a BLE device
+ * Disconnect from BLE device
+ */
+class DevicesPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}//DevicesPage
